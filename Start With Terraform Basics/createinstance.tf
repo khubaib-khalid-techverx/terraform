446987ac6e9633d960306b2f02b65139571ae9e5
khubaib-khalid-techverx <khubaib.khalid@techverx.com>
2021-05-24 11:39:38 +0500

    create machine in aws using terraform

--- Start With Terraform Basics/createinstance.tf
+++ Start With Terraform Basics/createinstance.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-    access_key = ""
-    secret_key = ""
-    region = ""
+    access_key = "AKIAYKY2QLYJHLYADFWR"
+    secret_key = "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"
+    region = "ap-south-1"
 }
 resource "aws_instance" "MyFirstInstance" {
   ami = "ami-0becdd21957bf5764"
