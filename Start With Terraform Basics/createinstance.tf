4f16c0151bdd1b2e91bc20eb53a222fb3d34548c
khubaib-khalid-techverx <khubaib.khalid@techverx.com>
2021-05-24 15:51:12 +0500

    separating configuration file

--- Start With Terraform Basics/createinstance.tf
+++ Start With Terraform Basics/createinstance.tf
@@ -1,8 +1,3 @@
-provider "aws" {
-    access_key = "AKIAYKY2QLYJHLYADFWR"
-    secret_key = "kkkkk"
-    region = "ap-south-1"
-}
 resource "aws_instance" "MyFirstInstance" {
   ami = "ami-0860c9429baba6ad2"
   instance_type = "t2.micro"
---
+++ Start With Terraform Basics/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+    access_key = "AKIAYKY2QLYJHLYADFWR"
+    secret_key = "kkkkk"
+    region = "ap-south-1"
+}
