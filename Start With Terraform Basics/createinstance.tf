8d73bfdbf14d8fe2fadbb899db1c8aeb0dec35a5
khubaib-khalid-techverx <khubaib.khalid@techverx.com>
2021-05-24 16:55:30 +0500

    Different names for every instance

--- Start With Terraform Basics/createinstance.tf
+++ Start With Terraform Basics/createinstance.tf
@@ -3,6 +3,6 @@ resource "aws_instance" "MyFirstInstance" {
   ami = "ami-0860c9429baba6ad2"
   instance_type = "t2.micro"
   tags = {
-    Name = "ThreeDemoInstances"
+    Name = "DemoInstances-${count.index}"
   }
 }
