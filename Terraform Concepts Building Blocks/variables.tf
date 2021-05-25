c68566b2c991ba3711ff013d78d48d3f0f27fefb
khubaib-khalid-techverx <khubaib.khalid@techverx.com>
2021-05-25 16:22:35 +0500

    changes in provider.tf

--- Terraform Concepts Building Blocks/variables.tf
+++ Terraform Concepts Building Blocks/variables.tf
@@ -1,12 +1,12 @@
 variable "AWS_ACCESS_KEY" {
     type = string
-    default = "AKIASMSIZOF42P2VUDSZ"
+    default = "AKIAYKY2QLYJDVQAEBMP"
 }
 
 variable "AWS_SECRET_KEY" {}
 
 variable "AWS_REGION" {
-default = "us-east-2"
+default = "ap-south-1"
 }
 
 variable "Security_Group"{
@@ -18,7 +18,7 @@ variable "AMIS" {
     type = map
     default = {
         us-east-1 = "ami-0f40c8f97004632f9"
-        us-east-2 = "ami-05692172625678b4e"
+        ap-south-1 = "ami-0860c9429baba6ad2"
         us-west-2 = "ami-0352d5a37fb4f603f"
         us-west-1 = "ami-0f40c8f97004632f9"
     }
