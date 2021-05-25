647303e873b710ff66269596f06d2b84a83fa08e
khubaib-khalid-techverx <khubaib.khalid@techverx.com>
2021-05-25 14:41:15 +0500

    Software provision with terraform

---
+++ Terraform Concepts Building Blocks/variables.tf
@@ -0,0 +1,34 @@
+variable "AWS_ACCESS_KEY" {
+  
+}
+
+variable "AWS_SECRET_KEY" {
+  
+}
+
+variable "AWS_REGION" {
+  default = "ap-south-1"
+}
+
+variable "Security_Group" {
+  type = list
+  default = ["sg-24076","sg-90890","sg-456789"]
+}
+
+variable "AMIS" {
+  type = map
+  default = {
+    ap-east-1 = "ami-013f864ad81b818d1"
+    ap-south-1 = "ami-03ab5f3b31d5ee063"
+    ap-southeast-1 = "ami-0f9d733050c9f5365"
+  }
+}
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "levelup_key"
+}
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "levelup_key.pub"
+}
+variable "INSTANCE_USERNAME" {
+    default = "ubuntu"
+}
