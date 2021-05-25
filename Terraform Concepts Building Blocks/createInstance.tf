13b8606e842406b92844c49bbf0e7e21f40d013d
khubaib-khalid-techverx <khubaib.khalid@techverx.com>
2021-05-25 16:02:16 +0500

    added provider file to software provision folder

--- Terraform Concepts Building Blocks/createInstance.tf
+++ Terraform Concepts Building Blocks/createInstance.tf
@@ -1,32 +1,35 @@
+
 resource "aws_key_pair" "levelup_key" {
-  key_name = "levelup_key"
-  public_key = file(var.PATH_TO_PUBLIC_KEY)
+    key_name = "levelup_key"
+    public_key = file(var.PATH_TO_PUBLIC_KEY)
 }
 
-resource "aws_instance" "MyFirstInstance" {
-  ami = lookup(var.AMIS,var.AWS_REGION)
+resource "aws_instance" "MyFirstInstnace" {
+  ami           = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
-  key_name = aws_key_pair.levelup_key.key_name
+  key_name      = aws_key_pair.levelup_key.key_name
+
   tags = {
-    Name = "Custom_Instances"
+    Name = "custom_instance"
   }
+
   provisioner "file" {
       source = "installNginx.sh"
-      destination ="/tmp/installNginx.sh"
+      destination = "/tmp/installNginx.sh"
   }
+
   provisioner "remote-exec" {
-      inline = [
-          "chmod +x /tmp/installNginx.sh",
-          "sudo sed -i -e 's/\r$//' /tmp/installNginx.sh",
-          "sudo /tmp/installNginx.sh",
-      ]
-  
+    inline = [
+      "chmod +x /tmp/installNginx.sh",
+      "sudo sed -i -e 's/\r$//' /tmp/installNginx.sh",  # Remove the spurious CR characters.
+      "sudo /tmp/installNginx.sh",
+    ]
   }
+
   connection {
-    host = coalesce(self.public_ip, self.private_ip)
-    type = "ssh"
-    user = var.INSTANCE_USERNAME
+    host        = coalesce(self.public_ip, self.private_ip)
+    type        = "ssh"
+    user        = var.INSTANCE_USERNAME
     private_key = file(var.PATH_TO_PRIVATE_KEY)
   }
-  #security_groups = "${var.Security_Group}"
 }
