d2566496510bfddfbac9faba2da046f22762d6fe
khubaib-khalid-techverx <khubaib.khalid@techverx.com>
2021-05-26 12:15:40 +0500

    filter ip ranges

---
+++ Terraform Concepts Building Blocks/Demo DataSource/securitygroup.tf
@@ -0,0 +1,19 @@
+data "aws_ip_ranges" "ap_south_ip_range" {
+  regions = ["ap-south-1","ap-southeast-1"]
+  services = ["ec2"]
+}
+
+resource "aws_security_group" "sg-custom_ap_south" {
+  name = "sg-custom_ap_south"
+  ingress = {
+      from_port = "443"
+      to_port = "443"
+      protocol = "tcp"
+      cidr_blocks = data.aws_ip_ranges.ap_south_ip_range.cidr_blocks
+  }
+
+  tags = {
+    CreateDate = data.aws_ip_ranges.ap_south_ip_range.create_date
+    SyncToken = data.aws_ip_ranges.ap_south_ip_range.sync_token
+  }
+}
